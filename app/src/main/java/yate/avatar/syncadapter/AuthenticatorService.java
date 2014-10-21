c60ab17eb1b6b5e84ebc46d0d9fa5ab7079565b1
gumanchu <freddy.gu@gmail.com>
2014-10-21 19:16:09 -0400

    Basic Authenticator classes for syncadapter (whatamidoinglol)

---
+++ app/src/main/java/yate/avatar/syncadapter/AuthenticatorService.java
@@ -0,0 +1,23 @@
+package yate.avatar.syncadapter;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+/**
+ * Created by Lou on 10/21/2014.
+ */
+public class AuthenticatorService extends Service {
+
+    private Authenticator mAuthenticator;
+
+    @Override
+    public void onCreate() {
+        mAuthenticator = new Authenticator(this);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mAuthenticator.getIBinder();
+    }
+}
